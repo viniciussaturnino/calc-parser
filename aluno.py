7255b05d205ddeb92ed96e8da169d0998309e69f
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-05-21 23:11:24 -0300

    First commit

--- aluno.py
+++ aluno.py
@@ -1,5 +1,5 @@
 # Identifique-se preenchendo as strings neste arquivo
-nome = "Nome Completo"
-matricula = "12/3456789"
-email = "alunx@email.com"
-usuario_github = "gituser"
+nome = "Vinicius de Sousa Saturnino"
+matricula = "18/0132245"
+email = "viniciussaturnino78@gmail.com"
+usuario_github = "vinicissaturnino"
